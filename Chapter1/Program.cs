63803ac4448d580575efe6f499e0a89b678f0898
daniel4191 <daniell8282@naver.com>
2026-02-11 22:13:54 +0900

    c# learning

---
+++ Chapter1/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, Daniel!");
+
+string myFriendsName;
+myFriendsName = "Judas";
+// Console.ReadKey();
+Console.WriteLine(myFriendsName);
+myFriendsName = "Frank";
+Console.WriteLine(myFriendsName);
