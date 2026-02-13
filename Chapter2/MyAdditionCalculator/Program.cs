4d305f66ba1ca28f047a467e3ef73847721d716d
daniel4191 <daniell8282@naver.com>
2026-02-13 21:59:57 +0900

    Daily C# prectice

--- Chapter2/MyAdditionCalculator/Program.cs
+++ Chapter2/MyAdditionCalculator/Program.cs
@@ -1,2 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+// string userInput = Console.ReadLine();
+// Console.WriteLine("You entered " + userInput);
+
+string myName = "Daniel";
+/* The line `// string userInput = Console.ReadLine();` is a commented-out line of code in C#. This
+means that it is not active or executed when the program runs. It is used for documentation or for
+temporarily disabling a line of code without deleting it. In this case, it appears to be a line of
+code that was previously used to read input from the console but has been commented out and is not
+currently being used in the program. */
+int Pigbo = 123;
+double numbers = 1.23;
+
+Console.WriteLine(myName, Pigbo, numbers);
+Console.WriteLine(Pigbo);
+Console.WriteLine(numbers);
+
+string petsName = "Nockchabo";
+System.Console.WriteLine($"my pet is {petsName}");
